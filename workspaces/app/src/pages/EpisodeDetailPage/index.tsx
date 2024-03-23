5aa12cc1de53e028d0dc4242e75e24736601a083
pomadev <y.pomadev@gmail.com>
2024-03-24 00:13:55 +0900

    fix: episode detail page

--- workspaces/app/src/pages/EpisodeDetailPage/index.tsx
+++ workspaces/app/src/pages/EpisodeDetailPage/index.tsx
@@ -3,9 +3,10 @@ import { useParams } from 'react-router-dom';
 import type { RouteParams } from 'regexparam';
 import invariant from 'tiny-invariant';
 
-import { useBook } from '../../features/book/hooks/useBook';
+// import { useBook } from '../../features/book/hooks/useBook';
+import { useEpisodeList } from '../../features/episode/hooks/useEpisodeList';
 import { EpisodeListItem } from '../../features/episode/components/EpisodeListItem';
-import { useEpisode } from '../../features/episode/hooks/useEpisode';
+// import { useEpisode } from '../../features/episode/hooks/useEpisode';
 import { Box } from '../../foundation/components/Box';
 import { Flex } from '../../foundation/components/Flex';
 import { Separator } from '../../foundation/components/Separator';
@@ -18,21 +19,22 @@ const EpisodeDetailPage: React.FC = () => {
   invariant(bookId);
   invariant(episodeId);
 
-  const { data: book } = useBook({ params: { bookId } });
-  const { data: episode } = useEpisode({ params: { episodeId } });
+  const { data: episodeList } = useEpisodeList({ query: { bookId } });
+  // const { data: book } = useBook({ params: { bookId } });
+  // const { data: episode } = useEpisode({ params: { episodeId } });
 
   return (
     <Box>
       <section aria-label="漫画ビューアー">
-        <ComicViewer episodeId={episode.id} />
+        <ComicViewer episodeId={episodeId} />
       </section>
 
       <Separator />
 
       <Box aria-label="エピソード一覧" as="section" px={Space * 2}>
         <Flex align="center" as="ul" direction="column" justify="center">
-          {book.episodes.map((episode) => (
-            <EpisodeListItem key={episode.id} bookId={bookId} episodeId={episode.id} />
+          {episodeList.map((episode) => (
+            <EpisodeListItem key={episode.id} bookId={bookId} episodeId={episode.id} image={episode.image} name={episode.name} chapter={episode.chapter} description={episode.description}  />
           ))}
         </Flex>
       </Box>
