bd8d51517098ff0666383b0185cb440ef968c703
pomadev <y.pomadev@gmail.com>
2024-03-23 22:56:50 +0900

    change: middleware

--- workspaces/server/src/routes/index.ts
+++ workspaces/server/src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Hono } from 'hono';
+import { compress } from 'hono/compress';
 import { cors } from 'hono/cors';
 import { HTTPException } from 'hono/http-exception';
 import { secureHeaders } from 'hono/secure-headers';
 
 import { cacheControlMiddleware } from '../middlewares/cacheControlMiddleware';
-import { compressMiddleware } from '../middlewares/compressMiddleware';
+// import { compressMiddleware } from '../middlewares/compressMiddleware';
 
 import { adminApp } from './admin';
 import { apiApp } from './api';
@@ -15,6 +16,7 @@ import { staticApp } from './static';
 const app = new Hono();
 
 app.use(secureHeaders());
+app.use(compress());
 app.use(
   cors({
     allowHeaders: ['Content-Type', 'Accept-Encoding', 'X-Accept-Encoding', 'Authorization'],
@@ -24,7 +26,7 @@ app.use(
     origin: (origin) => origin,
   }),
 );
-app.use(compressMiddleware);
+// app.use(compressMiddleware);
 app.use(cacheControlMiddleware);
 
 app.get('/healthz', (c) => {
